094fc639623e53427e27041150aed944e054b73e
Jenna Wang <291128221+stella-sage553@users.noreply.github.com>
2026-03-30 14:35:17 +0800

    refactor: hoist pooling helpers into utils.arrays

--- avlex/utils/__init__.py
+++ avlex/utils/__init__.py
@@ -1,6 +1,12 @@
 """Internal helpers shared across avlex modules."""
 
-from avlex.utils.arrays import l2_normalize, mean_pool, softmax
+from avlex.utils.arrays import (
+    avgpool_time,
+    l2_normalize,
+    mean_pool,
+    softmax,
+    stack_frames,
+)
 from avlex.utils.logging import get_logger
 from avlex.utils.seeding import derive_seed, rng_from_seed, seeded_matrix
 
@@ -8,6 +14,8 @@ __all__ = [
     "l2_normalize",
     "mean_pool",
     "softmax",
+    "avgpool_time",
+    "stack_frames",
     "get_logger",
     "derive_seed",
     "rng_from_seed",
