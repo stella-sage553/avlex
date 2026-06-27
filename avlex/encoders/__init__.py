ccc81620be3cc5d2c8c73c755a4ddf9f6e0634f4
Jenna Wang <291128221+stella-sage553@users.noreply.github.com>
2026-06-27 10:12:16 +0800

    feat: encoder registry

--- avlex/encoders/__init__.py
+++ avlex/encoders/__init__.py
@@ -1,5 +1,21 @@
 """Encoders: raw modality input -> temporal feature sequences."""
 
+from avlex.encoders.audio import EnergyEnvelopeEncoder, MelStatEncoder
 from avlex.encoders.base import Encoder
+from avlex.encoders.registry import (
+    available_encoders,
+    get_encoder,
+    register_encoder,
+)
+from avlex.encoders.visual import ColorStatsEncoder, MotionHistogramEncoder
 
-__all__ = ["Encoder"]
+__all__ = [
+    "Encoder",
+    "MotionHistogramEncoder",
+    "ColorStatsEncoder",
+    "MelStatEncoder",
+    "EnergyEnvelopeEncoder",
+    "get_encoder",
+    "register_encoder",
+    "available_encoders",
+]
